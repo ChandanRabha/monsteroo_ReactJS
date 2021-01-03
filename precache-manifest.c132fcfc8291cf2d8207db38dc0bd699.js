0028628a8be596b432d063c8972b60fa8cc6aa28
ChandanRabha <changrabha.rabha1@gmail.com>
2021-01-03 17:07:52 +0530

    Updates

---
+++ precache-manifest.c132fcfc8291cf2d8207db38dc0bd699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e4d57a02043721df116166058aa670",
+    "url": "/monsteroo_ReactJS/index.html"
+  },
+  {
+    "revision": "a84b1290e4dba238ad7d",
+    "url": "/monsteroo_ReactJS/static/css/main.473f6b36.chunk.css"
+  },
+  {
+    "revision": "5200f7acefcd2cd2bebc",
+    "url": "/monsteroo_ReactJS/static/js/2.d6f8af2a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsteroo_ReactJS/static/js/2.d6f8af2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84b1290e4dba238ad7d",
+    "url": "/monsteroo_ReactJS/static/js/main.453b87e6.chunk.js"
+  },
+  {
+    "revision": "1b1678b0c5db4e9ed84a",
+    "url": "/monsteroo_ReactJS/static/js/runtime-main.52d87b6a.js"
+  }
+]);
